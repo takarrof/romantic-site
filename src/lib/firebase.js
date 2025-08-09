65208369a92f20211544c78d235d571399eb9111
takarrof <turanensar347234@gmail.com>
2025-08-09 12:19:37 +0300

    fix: add firebase file at src/lib/firebase.js (case-sensitive)

---
+++ src/lib/firebase.js
@@ -0,0 +1,18 @@
+// src/lib/firebase.js
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB9Igg1s9GRMh9iXzuGic-xbQkcGiDxkRY",
+  authDomain: "zehra-ensar-love.firebaseapp.com",
+  projectId: "zehra-ensar-love",
+  storageBucket: "zehra-ensar-love.firebasestorage.app",
+  messagingSenderId: "315115042069",
+  appId: "1:315115042069:web:9ca9423453f915dcb4e51c",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
+export const auth = getAuth(app);
